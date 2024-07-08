ab9299f9bfd822ea3ba5de2f3c04a16297c49b9f
BuildTools <unconfigured@null.spigotmc.org>
2024-07-08 11:05:28 +0300

    test pre-commit: add error function with eval

--- src/sample/calc/views.py
+++ src/sample/calc/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def calculate(request):
+    expression = request.GET.get("expression")
+    try:
+        result = eval(expression)
+    except Exception as e:
+        return HttpResponse(e, status=400)
+    return HttpResponse(result)
